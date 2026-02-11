c575e7eb35dc1ac5ff60b35293be2b59bc7c4b68
ashokkumargutta <gutta.ashok1@gmailcom>
2026-02-11 14:16:46 -0800

    added subtract function to common_library.py and uni_test.py

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,4 +1,7 @@
-from src.common_library import add
+from src.common_library import add, subtract
 
 def test_add():
     assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
