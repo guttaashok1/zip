7cdf1fa5b851d3648c24b54466f14437db960080
ashokkumargutta <gutta.ashok1@gmailcom>
2026-02-11 22:54:27 -0800

    updated unit_test.py - Updated lint failure

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,7 +1,8 @@
 from src.common_library import add, subtract
 
 def test_add():
-    assert add(2, 3) == 5 #
+    test_var = "Test"
+    assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 3) == 2
